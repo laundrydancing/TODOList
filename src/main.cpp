13c096ea875bfcad4d6bd61312a5a994db003ae1
DanceInSea <username@users.noreply.github.com>
2024-11-30 19:46:31 +0800

    version1

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+    QIcon windowIcon(":/img/icon/icon5.ico");
+    a.setWindowIcon(windowIcon);
     MainWindow w;
     w.show();
     return a.exec();
